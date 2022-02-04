2ef72d149906fd633bc554c9b734b9e85f022ffc
Kyatsume <70340422+Kyatsume@users.noreply.github.com>
2022-02-05 02:01:04 +0800

    Update jquery.csv-calc.js

--- dist/jquery.csv-calc.js
+++ dist/jquery.csv-calc.js
@@ -140,7 +140,7 @@ $.extend(CsvCalc.prototype, /** @lends CsvCalc.prototype */ {
       
       // 合計を算出・表示
       var parent = $(ev.target).parents('[data-csvcalc-repeat]');
-      var price = Math.floor($(parent).find('[data-csvcalc-price]').text());
+      var price = $(parent).find('[data-csvcalc-price]').text();
       $(parent).find('[data-csvcalc-quotient]')
         .text(price / amount)
         .attr('data-csvcalc-quotient', price / amount));
