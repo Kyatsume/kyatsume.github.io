0086345556eb6d8c49943b88daa0a9fd82f46109
Kyatsume <70340422+Kyatsume@users.noreply.github.com>
2022-02-07 01:13:02 +0800

    Update jquery.csv-calc.js
    Add

--- dist/jquery.csv-calc.js
+++ dist/jquery.csv-calc.js
@@ -157,7 +157,7 @@ $.extend(CsvCalc.prototype, /** @lends CsvCalc.prototype */ {
   },
   calcTotal2: function () {
     var monster = this;
-    $(document).on('change', $(monster.elem).find('[data-csvcalc-mobsinput]'), {
+    $(document).on('change', $(monster.elem).find('[data-csvcalc-mobsinput]') {
       // バリデーションを行う
       var cMobs = monster.validateNumber.call(monster, $(ev.target).val());
       $(ev.target).val(cMobs); // 画面上の全角数字は、ここで半角となる。
