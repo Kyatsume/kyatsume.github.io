eda209fa3b7e50e2680a70f51f6567ddc1b2ede5
Kyatsume <70340422+Kyatsume@users.noreply.github.com>
2023-05-25 12:04:28 +0800

    Create search.js

---
+++ search.js
@@ -0,0 +1,51 @@
+function changeButtonText(text) {
+  // Change the text of the search button
+  document.querySelector('button[type="submit"]').innerHTML = text;
+}
+
+function getData() {
+  // Change the text of the search button to "Loading..."
+  changeButtonText("Loading...");
+
+  // Retrieve the value of the input field
+  var title = document.getElementById("title").value;
+
+  // Make the AJAX request
+  $.ajax({
+    url: "https://koalaxpanda.000webhostapp.com/playstationAPI/vendor/get_data.php",
+    type: "GET",
+    data: { title: title },
+    success: function(response) {
+      // Update the response div with the AJAX response
+      $("#response").html(response);
+
+      // Change the text of the search button back to "Search"
+      changeButtonText("Search");
+    },
+     error: function(response) {
+      // Update the response div with the AJAX response
+      $("#response").html(response);
+
+      // Change the text of the search button back to "Search"
+      changeButtonText("Search");
+    }
+  });
+
+  return false; // Prevent the form from submitting and refreshing the page
+}
+
+
+
+
+function getpsData() {
+  $.ajax({
+    url: "https://koalaxpanda.000webhostapp.com/playstationAPI/vendor/psplus.php",
+    type: "GET",
+    success: function(psresponse) {
+      $("#response").html(psresponse);
+    }
+  });
+}
+
+
+
