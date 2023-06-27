17d7391a92c3cb4c1f8533cfe15c752c91b528eb
Kyatsume <70340422+Kyatsume@users.noreply.github.com>
2023-06-27 10:42:08 +0800

    Update kmovies.php

--- kmovies.php
+++ kmovies.php
@@ -1,13 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>kmovies 24/7</title>
+    <title>Call PHP Script with JavaScript</title>
 </head>
 <body>
     <h1>Current Time: <?php echo (new DateTime('now', new DateTimeZone('Asia/Manila')))->format('H:i'); ?></h1>
+
+    <button onclick="loadPHP()">Load PHP Output</button>
     
-    <?php
-    include 'https://koalaxpanda.000webhostapp.com/kmovies.php';
-    ?>
+    <div id="output"></div>
+
+    <script>
+        function loadPHP() {
+            fetch('jhttps://koalaxpanda.000webhostapp.com/kmovies.php')
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('output').innerHTML = data;
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        }
+    </script>
 </body>
 </html>
